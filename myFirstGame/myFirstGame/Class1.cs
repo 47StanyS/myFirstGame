236d046d0d31c523e2a2cfb951ad9170a05b72bc
47StanyS <stanyslavalokhin@gmail.com>
2024-03-18 14:18:05 +0200

    I make statistics and greetings

---
+++ myFirstGame/myFirstGame/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirstGame
+{
+    internal class Class1
+    {
+        enum WeaponTyp
+        {
+            Rock = 1,
+            Scissors = 2,
+            Paper = 3,
+        }
+    }
+}
---
+++ myFirstGame/myFirstGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace myFirstGame
+{
+    class mainClass
+    {
+        static int roundsPlayed = 0;
+        static int victories = 0;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, what is your nicname?");
+            string nickname = Console.ReadLine();
+            Console.WriteLine($"{nickname}, how old are you?");
+            int age = int.Parse(Console.ReadLine());
+            if (age > 12) 
+            {
+                Console.WriteLine("OK!");
+                Console.WriteLine($"\nYour stats for this run:");
+                Console.WriteLine($"Nic: {nickname}");
+                Console.WriteLine($"Age: {age}");
+                Console.WriteLine($"Number of rounds played: {roundsPlayed}");
+                Console.WriteLine($"Number of wins: {victories}");
+            } 
+            else Console.WriteLine("The application can be used from 12 years old!");
+
+        }
+    }
+}
+
+
