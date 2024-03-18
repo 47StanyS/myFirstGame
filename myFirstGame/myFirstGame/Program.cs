c588d8117e0bcf096e8c665bd35d466f1e267746
47StanyS <stanyslavalokhin@gmail.com>
2024-03-18 15:51:01 +0200

    createYesNo

--- myFirstGame/myFirstGame/Program.cs
+++ myFirstGame/myFirstGame/Program.cs
@@ -13,7 +13,7 @@ namespace myFirstGame
             string nickname = Console.ReadLine();
             Console.WriteLine($"{nickname}, how old are you?");
             int age = int.Parse(Console.ReadLine());
-            if (age > 12) 
+            if (age >= 12) 
             {
                 Console.WriteLine("OK!");
                 Console.WriteLine($"\nYour stats for this run:");
@@ -21,10 +21,31 @@ namespace myFirstGame
                 Console.WriteLine($"Age: {age}");
                 Console.WriteLine($"Number of rounds played: {roundsPlayed}");
                 Console.WriteLine($"Number of wins: {victories}");
+                YesNo();
             } 
             else Console.WriteLine("The application can be used from 12 years old!");
-
         }
+        private static void YesNo()
+        {
+            bool yesNo = true;
+            while (yesNo)
+            {
+                Console.WriteLine("you vona go?\npress 'Y'or'N' ");
+                string choose = Console.ReadLine();
+                yesNo = choose.ToLower() == "yes" ? true : false;
+            }
+        }
+
+
+        /*private static void YesNo()
+        {
+            bool yesNo = true;
+            while (yesNo)
+            {
+                Console.WriteLine("you vona go? 'Yes' or 'No'");
+                yesNo = Console.ReadLine() == "y" ? true : false;
+            }
+        }*/
     }
 }
 
