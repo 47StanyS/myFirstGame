d849f66c0dbd1c1892e61fac1657b008965311bf
47StanyS <stanyslavalokhin@gmail.com>
2024-03-24 12:42:56 +0200

    ну)) з богом))

--- myFirstGame/myFirstGame/Program.cs
+++ myFirstGame/myFirstGame/Program.cs
@@ -31,6 +31,7 @@ namespace myFirstGame
             {
                 Console.WriteLine("Oh, you need to grow up.\nThe application can be used from 12 years old!");
                 Console.WriteLine($"Goodbye {nickname}");
+                Console.ReadLine();
                 return;
             }
             Console.WriteLine("Ouuu, you’re NOT as YOUNG as you seem at first glance.\nHere, take this book, it will come in handy on your adventure.");
@@ -43,7 +44,7 @@ namespace myFirstGame
                 Console.WriteLine("you vona go?\npress 'Yes'or'No' ");
                 string choose = Console.ReadLine().ToLower();
                 Console.WriteLine("_____________________________________________________________");
-                if (choose != "yes")
+                if (choose != "yes")    
                 {
                     Console.WriteLine($"Farewell, dear, {nickname},just RETuRN the BOOK I gave you, and go on your way in good health \n'{nickname}' AHAHHAH what's your name AHAHHA\noh i can't :P");
                     break;
@@ -87,7 +88,7 @@ namespace myFirstGame
                     roundsPlayed++;
                 }
 
-                if(wins == 2 || (wins == 2 && wins == 3))
+                if(wins >= 2)
                 {
                     Console.WriteLine("*********************************************");
                     Console.WriteLine("I knew YOU were CAPABLE of something!");
